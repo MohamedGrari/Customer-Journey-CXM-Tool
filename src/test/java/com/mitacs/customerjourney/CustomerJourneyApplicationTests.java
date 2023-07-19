8a0b48d21da99b77fc1b495aaefe171e2e05c9cf
MohamedGrari <graristar@gmail.com>
2023-07-19 15:01:37 -0400

    setup

---
+++ src/test/java/com/mitacs/customerjourney/CustomerJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitacs.customerjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
